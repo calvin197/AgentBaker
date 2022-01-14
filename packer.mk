c072f3a3d658edb9f20dfd3f0ef41d16f6246558
Charlie Li <charlili@microsoft.com>
2022-01-13 23:12:03 -0800

    use private packer to workaround arm64 restriction

--- packer.mk
+++ packer.mk
@@ -5,8 +5,10 @@ ifeq (${OS_SKU},Ubuntu)
 ifeq (${ARCHITECTURE},ARM64)
 ifeq (${MODE},gen2Mode)
 	@echo "${MODE}: Building with Hyper-v generation 2 ARM64 VM"
-	@packer build -var-file=vhdbuilder/packer/settings.json vhdbuilder/packer/vhd-image-builder-arm64-gen2.json
-	@echo "${MODE}: Building with Hyper-v generation 2 ARM64 VM - completed"
+	@rm -f ./vhdbuilder/packer/packer
+	@curl -fsSL https://packerbinary.blob.core.windows.net/bin/packer -o ./vhdbuilder/packer/packer
+	@chmod a+x ./vhdbuilder/packer/packer
+	@./vhdbuilder/packer/packer build -var-file=vhdbuilder/packer/settings.json vhdbuilder/packer/vhd-image-builder-arm64-gen2.json
 else ifeq (${MODE},sigMode)
 	$(error sigMode not supported yet)
 else
