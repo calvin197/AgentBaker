6577ebdfd6d2c3cbe545d49d940255d67e6368a5
Charlie Li <charlili@microsoft.com>
2022-01-14 14:46:45 -0800

    merge two steps to one step

--- vhdbuilder/packer/convert-osdisk-snapshot-to-sig.sh
+++ vhdbuilder/packer/convert-osdisk-snapshot-to-sig.sh
@@ -5,8 +5,8 @@ required_env_vars=(
     "AZURE_RESOURCE_GROUP_NAME"
     "AZURE_LOCATION"
     "SIG_IMAGE_NAME"
-    "SIG_IMAGE_VERSION"
     "ARM64_OS_DISK_SNAPSHOT_NAME"
+    "CREATE_TIME"
 )
 
 for v in "${required_env_vars[@]}"
@@ -20,5 +20,5 @@ done
 disk_snapshot_id="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${AZURE_RESOURCE_GROUP_NAME}/providers/Microsoft.Compute/snapshots/${ARM64_OS_DISK_SNAPSHOT_NAME}"
 
 az sig image-version create --location $AZURE_LOCATION --resource-group ${AZURE_RESOURCE_GROUP_NAME} --gallery-name PackerSigGalleryEastUS \
-     --gallery-image-definition ${SIG_IMAGE_NAME} --gallery-image-version ${SIG_IMAGE_VERSION} \
+     --gallery-image-definition ${SIG_IMAGE_NAME} --gallery-image-version 1.0.${CREATE_TIME} \
      --os-snapshot ${disk_snapshot_id}
