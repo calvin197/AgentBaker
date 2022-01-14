adf86537e25dd6d2b77c044f8496295c586c6067
Charlie Li <charlili@microsoft.com>
2022-01-14 09:01:37 -0800

    shortcut the installation for quick tests

--- vhdbuilder/packer/install-dependencies.sh
+++ vhdbuilder/packer/install-dependencies.sh
@@ -33,6 +33,7 @@ if [[ $OS == $MARINER_OS_NAME ]]; then
 fi
 
 copyPackerFiles
+exit 0
 
 echo ""
 echo "Components downloaded in this VHD build (some of the below components might get deleted during cluster provisioning if they are not needed):" >> ${VHD_LOGS_FILEPATH}
