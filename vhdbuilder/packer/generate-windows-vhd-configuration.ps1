26de1870ab4e74b5f99ee0aae4373f3cbec2fff5
ShiqianTao <shtao@microsoft.com>
2022-01-20 11:20:13 +0800

    update iamgeToPull

--- vhdbuilder/packer/generate-windows-vhd-configuration.ps1
+++ vhdbuilder/packer/generate-windows-vhd-configuration.ps1
@@ -84,29 +84,38 @@ switch ($windowsSKU) {
     }
     "2022-containerd" {
         $global:imagesToPull = @(
+            "mcr.microsoft.com/windows/server:ltsc2022",
             "mcr.microsoft.com/windows/servercore:ltsc2022",
-            "mcr.microsoft.com/windows/nanoserver:ltsc2022",
-            # "mcr.microsoft.com/oss/kubernetes/pause:3.4.1", # TODO
-            "mcr.microsoft.com/oss/kubernetes/pause:3.6-hotfix.20220114", # TODO
-            # "mcr.microsoft.com/oss/kubernetes-csi/livenessprobe:v2.4.0", # TODO
-            "mcr.microsoft.com/oss/kubernetes-csi/livenessprobe:v2.5.0", # TODO
-            "mcr.microsoft.com/oss/kubernetes-csi/csi-node-driver-registrar:v2.3.0", # TODO
-            "mcr.microsoft.com/oss/kubernetes-csi/csi-node-driver-registrar:v2.4.0", # TODO
-            # "mcr.microsoft.com/oss/kubernetes-csi/azuredisk-csi:v1.8.0", # TODO
-            "mcr.microsoft.com/oss/kubernetes-csi/azuredisk-csi:v1.10.0", # TODO
-            # "mcr.microsoft.com/oss/kubernetes-csi/azurefile-csi:v1.7.0", # TODO
-            "mcr.microsoft.com/oss/kubernetes-csi/azurefile-csi:v1.9.0", # TODO
-            "mcr.microsoft.com/oss/kubernetes-csi/secrets-store/driver:v1.0.0",
-            "mcr.microsoft.com/oss/azure/secrets-store/provider-azure:v1.0.0",
-            # "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.6.0", # for k8s 1.19.x
-            # "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.7.9", # for k8s 1.20.x
-            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.7.11", # for k8s 1.20.x
-            # "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.0.6", # for k8s 1.21.x
-            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.0.8", # for k8s
-            # "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.1.2", # for k8s 1.22.x
-            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.1.4", # for k8s 1.22.x
-            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.23.1", # for k8s 1.23.x
-            "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:win-ciprod10132021" # TODO
+            "mcr.microsoft.com/windows/nanoserver:2022",
+            "mcr.microsoft.com/windows/nanoserver:ltsc2022", # Not need?
+            "mcr.microsoft.com/oss/kubernetes/pause:3.4.1", # Can not pull
+            "mcr.microsoft.com/oss/kubernetes/pause:3.6-hotfix.20220114", # Not need?
+            "mcr.microsoft.com/oss/kubernetes-csi/livenessprobe:v2.3.0",
+            "mcr.microsoft.com/oss/kubernetes-csi/livenessprobe:v2.4.0", # Can not pull
+            "mcr.microsoft.com/oss/kubernetes-csi/livenessprobe:v2.5.0", # Not need?
+            "mcr.microsoft.com/oss/kubernetes-csi/csi-node-driver-registrar:v2.2.0",
+            "mcr.microsoft.com/oss/kubernetes-csi/csi-node-driver-registrar:v2.3.0",
+            "mcr.microsoft.com/oss/kubernetes-csi/csi-node-driver-registrar:v2.4.0", # Not need?
+            "mcr.microsoft.com/oss/kubernetes-csi/azuredisk-csi:v1.7.0",
+            "mcr.microsoft.com/oss/kubernetes-csi/azuredisk-csi:v1.8.0", # Can not pull
+            "mcr.microsoft.com/oss/kubernetes-csi/azuredisk-csi:v1.10.0", # Not need?
+            "mcr.microsoft.com/oss/kubernetes-csi/azurefile-csi:v1.6.0",
+            "mcr.microsoft.com/oss/kubernetes-csi/azurefile-csi:v1.7.0", # Can not pull
+            "mcr.microsoft.com/oss/kubernetes-csi/azurefile-csi:v1.9.0", # Not need?
+            "mcr.microsoft.com/oss/azure/secrets-store/provider-azure:0.0.14",
+            "mcr.microsoft.com/oss/azure/secrets-store/provider-azure:v1.0.0", # Email
+            "mcr.microsoft.com/oss/kubernetes-csi/secrets-store/driver:v0.0.21",
+            "mcr.microsoft.com/oss/kubernetes-csi/secrets-store/driver:v1.0.0", # Email
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.6.0",  # for k8s 1.19.x # Can not pull
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.7.9",  # for k8s 1.20.x # Can not pull
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v0.7.11", # for k8s 1.20.x # Not need?
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.0.6",  # for k8s 1.21.x # Can not pull
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.0.8",  # for k8s X.XX.X # Not need?
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.1.2",  # for k8s 1.22.x # Can not pull
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.1.4",  # for k8s 1.22.x # Not need?
+            "mcr.microsoft.com/oss/kubernetes/azure-cloud-node-manager:v1.23.1", # for k8s 1.23.x # Not need?
+            "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:win-ciprod06112021-2",
+            "mcr.microsoft.com/azuremonitor/containerinsights/ciprod:win-ciprod10132021"
         )
     }
     default {
