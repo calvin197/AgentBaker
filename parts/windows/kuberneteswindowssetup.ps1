63515c1b854206beeeb51683342febaba99fb480
Calvin Shum <calvin197@ymail.com>
2023-10-10 12:38:39 -0700

    fix typo

--- parts/windows/kuberneteswindowssetup.ps1
+++ parts/windows/kuberneteswindowssetup.ps1
@@ -464,11 +464,6 @@ try
         Start-InstallGPUDriver
     }
 
-    if ($global:ConfigGPUDriverIfNeeded) {
-        Write-Log "Start GPU installation"
-        Start-InstallGPUDriver
-    }
-
     if (Test-Path $CacheDir)
     {
         Write-Log "Removing aks-engine bits cache directory"
